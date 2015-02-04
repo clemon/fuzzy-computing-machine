c5dd86ffae3ce33bc036331348b4133f29ebb056
Chet Lemon <clemon@ucsd.edu>
2015-02-03 22:08:43 -0800

    regfile done i think

--- regfile.sv
+++ regfile.sv
@@ -1,7 +1,30 @@
 module regfile(
-	input sourceReg1_i,
-	input sourceReg2_i,
-	input destReg_i,
-	input regwrite_i,
-	input clk
+	input [2:0] sourceReg1_i,
+	input [2:0] sourceReg2_i,
+	input [2:0] destReg_i,
+	input writeFlag_i,
+	input [7:0] data_i,
+	input clk,
+	output reg [7:0] data1_o,
+	output reg [7:0] data2_o
 );
+
+reg [7:0] registers [7:0];
+
+//assign data1_o = registers[sourceReg1_i];
+//assign data2_o = registers[sourceReg2_i];
+
+always_ff @ (posedge clk) 
+begin
+	if (writeFlag == 1)
+	begin
+		registers[destReg_i] <= data_i;
+	end
+end
+
+always_comb 
+begin
+	data1_o = registers[sourceReg1_i];
+	data2_o = registers[sourceReg2_i];
+end
+endmodule
