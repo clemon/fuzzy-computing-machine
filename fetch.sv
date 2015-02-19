8c9f81f417c9336586d01ea7f1b985e51f39e35f
Mathian Directo <mccirect@ucsd.edu>
2015-02-18 21:55:21 -0800

    Fixed fetch output, is now PC instead of nextPC

--- fetch.sv
+++ fetch.sv
@@ -4,10 +4,10 @@ module fetch (
 	input [7:0] start_address_i,
 	input branch_i,	// whether or not to branch
 	input [7:0] branchloc_i,//Where to branch
-	output reg [7:0] next
+	output reg [7:0] pc
 );
 
-reg [7:0] pc;
+reg [7:0] next;
 reg started;
 
 always_comb
