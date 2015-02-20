da1cda67b5be867452fd9d17bb85bedfcaf6bf01
Chet Lemon <clemon@ucsd.edu>
2015-02-19 20:36:14 -0800

    made a mux yey

---
+++ mux.sv
@@ -0,0 +1,16 @@
+module mux(
+	input [7:0] input1,
+	input [7:0] input2,
+	input sel,
+	output [7:0] muxout
+);
+
+always_comb begin
+
+	if(sel)
+		muxout = input2;
+	else
+		muxout = input1;
+		
+end
+endmodule
